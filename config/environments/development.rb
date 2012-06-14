da900656b29deff1333f714bb8119d4c51950833
James Carbine <JamesCarbine@gmail.com>
2012-06-14 14:38:10 -0600

    changed config to compile once at server startup, not at each page change.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
@@ -11,7 +11,9 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
+
+config.action_view.cache_template_loading = true
 
 # run rake js:build to build the optimized JS if set to true
 # ENV['USE_OPTIMIZED_JS']                            = 'true'
