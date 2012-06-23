0b77db215501f1e5bd8d12d4b14758e99e9997d8
James Carbine <JamesCarbine@gmail.com>
2012-06-22 23:08:50 -0600

    can now edit the misconception name on the index page

--- app/controllers/misconceptions_controller.rb
+++ app/controllers/misconceptions_controller.rb
@@ -11,6 +11,16 @@ class MisconceptionsController < ApplicationController
   def create
   end
 
+  def update
+    @misconception = Misconception.find(params[:id])
+    if @misconception.update_attributes(params[:quiz_misconception])
+      @misconception.reload
+      render :json => @misconception.to_json
+    else
+      render :json => @misconception.errors.to_json, :status => :bad_request
+    end
+  end
+
   def index
     @abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     add_crumb("Misconceptions")
